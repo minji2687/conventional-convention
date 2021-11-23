cec6181e919294044897c0c5b3036ffa4575e360
Jo Minji <mingi2687@gamil.com>
2021-11-23 17:30:56 +0900

    feat: Add 3 list
    To test traing comma

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,3 +1,16 @@
+fruits = ['Apple', 'Banana', 'Pineapple']
+
+animals = [
+   'Lion',
+   'Tiger',
+   'Rabit'
+]
+
+Clubs = [
+   'MenuU',
+   'ManC',
+]
+
 for i in range(1,16+1):
 	if i%15==0:
 		print('fizzbuzz')
